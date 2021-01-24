3e38ee581cdb4eb06b95c28caaab8c0f954e445f
khushal540 <77763138+khushal540@users.noreply.github.com>
2021-01-24 11:30:11 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+
+namespace ClientServer1.Libraries
+{
+    public class Dnslib
+    {
+        public void TestUri()
+        {
+            var uri = GetASimpleUri();
+
+            Console.WriteLine("Uri: {0}", uri.ToString());
+            
+        }
+        public void GetDnsInfo(string webDomainName)
+        {
+            var domain = Dns.GetHostEntry(webDomainName);
+
+            Console.WriteLine("Hostname: {0}", domain.HostName);
+            foreach (var a in domain.AddressList)
+            {
+                Console.WriteLine("Addresslist: {0}", a);
+            }
+
+        }
+        private Uri GetASimpleUri()
+        {
+            var builder = new UriBuilder();
+            builder.Scheme = "http";
+            builder.Host = "google.com";
+
+            return builder.Uri;
+        }
+    
+}
+       public class TestUri
+		{
+		public static void Main()
+		{
+		Dnslib d=new Dnslib();
+		d.TestUri();
+		d.GetDnsInfo("www.google.Com");
+		}
+		}
+}
+
+
+
